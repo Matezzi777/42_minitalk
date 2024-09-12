6af7c17656effc00bba093a11693add5eca77815
Matezzi777 <matezzi777@gmail.com>
2024-09-12 02:06:14 +0200

    display_server_startup_message() added

--- server.c
+++ server.c
@@ -6,13 +6,30 @@
 /*   By: mmartina <mmartina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 16:48:34 by mmartina          #+#    #+#             */
-/*   Updated: 2024/09/12 00:52:46 by mmartina         ###   ########.fr       */
+/*   Updated: 2024/09/12 02:02:16 by mmartina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 unsigned int	g_count = 0;
+
+void	display_server_startup_message(int pid)
+{
+	ft_printf("███╗   ███╗██╗███╗   ██╗██╗████████╗ █████╗ ██╗     ██╗  ██╗\n");
+	ft_printf("████╗ ████║██║████╗  ██║██║╚══██╔══╝██╔══██╗██║     ██║ ██╔╝\n");
+	ft_printf("██╔████╔██║██║██╔██╗ ██║██║   ██║   ███████║██║     █████╔╝\n");
+	ft_printf("██║╚██╔╝██║██║██║╚██╗██║██║   ██║   ██╔══██║██║     ██╔═██╗\n");
+	ft_printf("██║ ╚═╝ ██║██║██║ ╚████║██║   ██║   ██║  ██║███████╗██║  ██╗\n");
+	ft_printf("╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝\n\n");
+	ft_printf("      ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗       \n");
+	ft_printf("      ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗      \n");
+	ft_printf("█████╗███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝█████╗\n");
+	ft_printf("╚════╝╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██╔══██╗╚════╝\n");
+	ft_printf("      ███████║███████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║      \n");
+	ft_printf("      ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝      \n");
+	ft_printf("PID : %d\n\n", pid);
+}
 
 void	handle_signal(int code)
 {
@@ -28,7 +45,8 @@ void	handle_signal(int code)
 	{
 		if (actual_char == 0)
 		{
-			ft_printf("\nTRANSMISSION TERMINEE\n\nEn attente d'un signal...\n");
+			ft_printf("\n\n===== TRANSMISSION TERMINEE =====\n\n\nEn attente...\n");
+			ft_printf("===== PRINT AREA =====\n\n");
 		}
 		else
 			ft_putchar_fd(actual_char, 1);
@@ -44,8 +62,8 @@ int	main(void)
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-	ft_printf("Server launched with PID : %d\nEn attente d'un signal...\n",
-		getpid());
+	display_server_startup_message(getpid());
+	ft_printf("En attente...\n===== PRINT AREA =====\n\n");
 	while (1)
 		pause();
 }
