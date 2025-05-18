64550a036598b7a2e7c5c98c78a19aeda9d8f14d
Matezzi777 <matezzi777@gmail.com>
2025-05-18 23:47:19 +0200

    Project finished (+ bonus)

--- srcs/client.c
+++ srcs/client.c
@@ -6,22 +6,66 @@
 /*   By: maxmart2 <maxmart2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 22:53:24 by maxmart2          #+#    #+#             */
-/*   Updated: 2025/05/18 02:12:51 by maxmart2         ###   ########.fr       */
+/*   Updated: 2025/05/18 23:44:35 by maxmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
 #include "libft.h"
+#include <signal.h>
+
+volatile sig_atomic_t	g_acknowledge = 0;
+
+void	sig_handler(int sig)
+{
+	(void)sig;
+	g_acknowledge = 1;
+	if (sig == SIGUSR2)
+		ft_printf("\033[32mMessage successfuly sent!\n\033[0m");
+}
+
+void	send_character(int pid, unsigned char c)
+{
+	int	i;
+
+	i = 8;
+	while (--i >= 0)
+	{
+		if ((c >> i) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		while (!g_acknowledge)
+			pause();
+		g_acknowledge = 0;
+	}
+}
+
+void	send_message(int pid, char *message)
+{
+	int	i;
+
+	i = -1;
+	while (message[++i])
+		send_character(pid, (unsigned char)message[i]);
+	send_character(pid, '\0');
+}
 
 int	main(int argc, char **argv)
 {
-	int	server_pid;
+	pid_t				server_pid;
+	struct sigaction	sa;
 
 	if (argc != 3)
 		ft_printf("Wrong nb of arguments.\nUse : ./client <PID> <string>\n");
 	else
 	{
-		server_pid = ft_atoi(argv[1]);
-		ft_printf("Sending \"%s\" to server %d\n", argv[2], server_pid);
+		server_pid = (pid_t)ft_atoi(argv[1]);
+		sa.sa_handler = &sig_handler;
+		sigemptyset(&sa.sa_mask);
+		sa.sa_flags = 0;
+		sigaction(SIGUSR1, &sa, NULL);
+		sigaction(SIGUSR2, &sa, NULL);
+		ft_printf("Sending \"%s\" to server %d.\n\n", argv[2], server_pid);
+		send_message(server_pid, argv[2]);
 	}
 }
--- srcs/server.c
+++ srcs/server.c
@@ -6,19 +6,79 @@
 /*   By: maxmart2 <maxmart2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 22:53:27 by maxmart2          #+#    #+#             */
-/*   Updated: 2025/05/18 02:09:51 by maxmart2         ###   ########.fr       */
+/*   Updated: 2025/05/18 23:42:24 by maxmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
 #include "libft.h"
+#include <signal.h>
+
+volatile sig_atomic_t	g_lezgo = 0;
+
+void	ft_put_logo(void)
+{
+	ft_printf("\033[31m ▄▄   ▄▄  ▄▄▄  ▄▄    ▄  ▄▄▄  ▄▄▄▄▄▄▄  ▄▄▄▄▄▄  ▄▄▄      "
+		"▄▄▄   ▄ \n");
+	ft_printf("\033[32m█  █▄█  ██   ██  █  █ ██   ██       ██      ██   █    █"
+		"   █ █ █\n");
+	ft_printf("\033[33m█       ██   ██   █▄█ ██   ██▄     ▄██  ▄   ██   █    █"
+		"   █▄█ █\n");
+	ft_printf("\033[34m█       ██   ██       ██   █  █   █  █ █▄█  ██   █    █"
+		"      ▄█\n");
+	ft_printf("\033[35m█       ██   ██  ▄    ██   █  █   █  █      ██   █▄▄▄ █"
+		"     █▄\n");
+	ft_printf("\033[36m█ ██▄██ ██   ██ █ █   ██   █  █   █  █  ▄   ██       ██"
+		"    ▄  █\n");
+	ft_printf("\033[37m█▄█   █▄██▄▄▄██▄█  █▄▄██▄▄▄█  █▄▄▄█  █▄█ █▄▄██▄▄▄▄▄▄▄██"
+		"▄▄▄█ █▄█\n\033[0m");
+}
+
+void	sig_handler(int sig, siginfo_t *info, void *context)
+{
+	static int				bit_index = 0;
+	static unsigned char	current_char = 0;
+
+	(void)context;
+	if (sig == SIGUSR1)
+		current_char |= (1 << (7 - bit_index));
+	bit_index++;
+	if (bit_index == 8)
+	{
+		if (current_char == '\0')
+		{
+			kill(info->si_pid, SIGUSR2);
+			g_lezgo = 1;
+		}
+		else
+			write(1, &current_char, 1);
+		bit_index = 0;
+		current_char = 0;
+	}
+	kill(info->si_pid, SIGUSR1);
+}
 
 int	main(void)
 {
-	ft_printf("PID : %d\n", getpid());
+	struct sigaction	sa;
+
+	sa.sa_sigaction = &sig_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_put_logo();
+	ft_printf("\n   ================\n   | PID : %d |\n"
+		"   ================\n\n", getpid());
 	while (TRUE)
 	{
-		ft_printf("Waiting for signals...\n");
-		sleep(1);
+		ft_printf("Waiting for signals...\n\n\033[34m");
+		while (TRUE)
+		{
+			pause();
+			if (g_lezgo)
+				break ;
+		}
+		g_lezgo = 0;
+		ft_printf("\n\n\033[0m");
 	}
 }
